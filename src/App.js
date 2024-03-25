ac851f0674f457113b8b6278687894b6633df135
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-03-25 15:38:36 +0100

    Exercices pour le cours de React (SIELI)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0)
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
+
+  const formObject = {
+    FirstName: firstName,
+    LastName: lastName,
+    Email: email,
+    Message: message
+  }
+
+  const dataStorage = () => {
+    for (let item in formObject) {
+      sessionStorage.setItem(item, formObject[item])
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <button onClick={() => setCount(count-1)}>-</button>
+        <div>{count}</div>
+        <button onClick={() => setCount(count+1)}>+</button>
+      </div>
+      <div className='div-form'>
+        <form onSubmit={dataStorage}>
+          <label>
+            Prénom :
+            <input className='input-form' type="text" value={firstName} onChange={e => setFirstName(e.target.value)} required />
+            Nom :
+            <input className='input-form' type="text" value={lastName} onChange={e => setLastName(e.target.value)} required />
+            Email :
+            <input className='input-form' type="email" value={email} onChange={e => setEmail(e.target.value)} required />
+            Message :
+            <input className='input-form' type="text" value={message} onChange={e => setMessage(e.target.value)} required />
+          </label>
+          <input className='btn-submit' type="submit" value="Envoyer" />
+        </form>
+      </div>
+    </>
   );
 }
 
